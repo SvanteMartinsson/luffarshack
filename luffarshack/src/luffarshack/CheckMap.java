cf42d298fcd4e417278cd6659cf2897a94413309
SvanteMartinsson <99masv@skola.engelholm.se>
2016-12-13 19:30:14 +0100

    boi

--- luffarshack/src/luffarshack/CheckMap.java
+++ luffarshack/src/luffarshack/CheckMap.java
@@ -7,7 +7,7 @@ public class CheckMap {
 	public void checkPlayer(int[][] map, int height, int width){
 		for(int y = 0; y < height; y++){
 			for(int x = 0; x<width; x++){
-				// Kod för att kolla om spelaren vinner här
+				
 			}
 		}
 	}
