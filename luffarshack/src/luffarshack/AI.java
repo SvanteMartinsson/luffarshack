d1c5c08cf3b856ff6352979334b98a1f31be6ca8
Simon Haglund <simonhaglund99@gmail.com>
2016-12-15 11:22:29 +0100

    AI nigger

--- luffarshack/src/luffarshack/AI.java
+++ luffarshack/src/luffarshack/AI.java
@@ -2,6 +2,27 @@ package luffarshack;
 
 public class AI {
 
+	/**
+
+	* Kolla P1 vinst
+	* Kolla AI vinst
+	* Kolla om P1 har 4 i rad en sida oblockad => block
+	* Kolla om AI har 3 i rad !block => offense
+	* Kolla om P1 3 i rad ! block => block
+	* Kolla om AI är ett drag från 3 i rad på två håll => offense
+	* Kolla om P1 är ett drag från 3 i rad på två håll => block
+	* Kolla om AI har 4 i rad !dubbelblock => offense vinst
+	* Kolla om AI har 3 i rad åt något håll !block => Offense
+	* Kolla om AI är 1move från dubbla 3 i rad => offense
+	* Kolla om AI är 1move från singel 3 irad => offense
+	* Kolla om P1 är 1move från 3 i rad => block
+	* Kolla om P1 är 1move från 2 i rad => block
+	* Kolla om AI är 1move från 2 i rad => offense
+	* Kolla om AI är 
+	* Gå offensivt, försök hitta opportunity för att få 3 i rad på två håll
+	* 
+	*/
+	
 	private int x;
 	private int y;
 	private int width;
